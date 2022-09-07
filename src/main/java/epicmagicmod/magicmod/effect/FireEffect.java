83f644a742a26188edc48386c764b7a8fbd368d4
DeceasedWolf <saltcoffee.mathis@gmail.com>
2022-09-07 18:06:21 -0400

    pls no break

--- src/main/java/epicmagicmod/magicmod/effect/FireEffect.java
+++ src/main/java/epicmagicmod/magicmod/effect/FireEffect.java
@@ -1,13 +1,13 @@
 package epicmagicmod.magicmod.effect;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
 
@@ -17,7 +17,7 @@ public class FireEffect extends MobEffect {
     }
 
 
-    private void setFire(Level level, Player player){
+    private void setFire(Level level, LivingEntity player){
 
         BlockPos playerPos = player.blockPosition();
         AABB area = new AABB(playerPos.getX() - 7, playerPos.getY() - 1, playerPos.getZ() - 7, playerPos.getX() + 7, playerPos.getY() + 2, playerPos.getZ() + 7);
@@ -27,10 +27,8 @@ public class FireEffect extends MobEffect {
             if (e instanceof LivingEntity living) {
                 //   player.sendSystemMessage(Component.literal("we did a different thing hopefully to" + living.getName()));
 
-                living.setSecondsOnFire(15);
+                living.hurt(DamageSource.OUT_OF_WORLD, 0.80f);
 
-                living.forceAddEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 2, 2), living);
-                living.forceAddEffect(new MobEffectInstance(MobEffects.BLINDNESS, 2, 1), living);
             }
 
         }
@@ -46,13 +44,14 @@ public class FireEffect extends MobEffect {
 
                     case 0:
                         pLivingEntity.forceAddEffect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 5, 1), pLivingEntity);
-                        pLivingEntity.forceAddEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 5, 1), pLivingEntity);
+                        pLivingEntity.forceAddEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 5, 0), pLivingEntity);
                         break;
 
 
                     case 1:
+                        setFire(pLivingEntity.level, pLivingEntity);
                         pLivingEntity.forceAddEffect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 5, 1), pLivingEntity);
-                        pLivingEntity.forceAddEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 5, 2), pLivingEntity);
+                        pLivingEntity.forceAddEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 5, 1), pLivingEntity);
                         break;
 
 
--- src/main/java/epicmagicmod/magicmod/items/wands/LifeStealWand.java
+++ src/main/java/epicmagicmod/magicmod/items/wands/LifeStealWand.java
@@ -1,8 +1,24 @@
 package epicmagicmod.magicmod.items.wands;
 
 import epicmagicmod.magicmod.block.ShardOreItem;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Stack;
 
 public class LifeStealWand extends WandParent{
     public LifeStealWand(Properties properties, int mainManaUsage, int altManaUsage, String name, float costMult, ShardOreItem.EOreType bound) {
@@ -10,12 +26,61 @@ public class LifeStealWand extends WandParent{
     }
 
     @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        activate(level, player);
+        return super.use(level, player, hand);
+    }
+
+    @Override
     public boolean mainAbility(Level level, Player player) {
-        return false;
+        //(x,y,z,x,y,z) middle is the player
+        int lvl = getLVL(player.getItemInHand(InteractionHand.MAIN_HAND));
+        int l = (lvl+1) * 2;
+        BlockPos playerPos = player.blockPosition();
+
+        AABB area = new AABB(playerPos.getX()-l, playerPos.getY()-1, playerPos.getZ()-l, playerPos.getX()+l, playerPos.getY()+2, playerPos.getZ()+l);
+        List<Entity>ents = level.getEntities(player, area);
+        if (ents.size() == 0){
+            return false;
+        }
+        for(Entity e : ents){
+
+            if (e instanceof LivingEntity living){
+                //   player.sendSystemMessage(Component.literal("we did a different thing hopefully to" + living.getName()));
+
+                living.setHealth(living.getHealth() - l);
+                player.heal(ents.size()*2);
+
+            }
+
+        }
+        return true;
     }
 
     @Override
     public boolean altAbility(Level level, Player player) {
-        return false;
+
+        int lvl = getLVL(player.getItemInHand(InteractionHand.MAIN_HAND));
+        int l = (lvl+1) * 2;
+        BlockPos playerPos = player.blockPosition();
+
+        AABB area = new AABB(playerPos.getX()-l, playerPos.getY()-1, playerPos.getZ()-l, playerPos.getX()+l, playerPos.getY()+2, playerPos.getZ()+l);
+        List<Entity>ents = level.getEntities(player, area);
+        if (ents.size() == 0){
+            return false;
+        }
+        for(Entity e : ents){
+
+            if (e instanceof LivingEntity living){
+                //   player.sendSystemMessage(Component.literal("we did a different thing hopefully to" + living.getName()));
+
+                living.forceAddEffect(new MobEffectInstance(MobEffects.POISON, 15, 2), living);
+                living.hurt(DamageSource.OUT_OF_WORLD, 5f);
+
+            }
+
+        }
+        return true;
+
     }
 }
